d7b86ea7e20f2daa58eaad2f3181c48f90142c81
rich0024 <yance0022@gmail.com>
2021-01-01 16:52:44 -0500

    user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        if users
+            render json: users
+        else 
+            render json: {
+                error: 'no users found'
+            }, status: 500
+        end
+    end
+
+    def show
+        user = User.find(params[:id])
+        if user
+            render json: user
+        else
+            render json: {
+                status: 500,
+                errors: ['no users found']
+            }
+        end
+    end
+
+    def create
+        user = User.new(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :balance)
+    end
 end
